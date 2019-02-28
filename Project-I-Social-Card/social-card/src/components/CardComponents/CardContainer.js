78c0f6ede82cdcef4e798b59ff748ba8f7a6f2ca
Melky <melky@Melkys-iMac.local>
2019-02-27 23:26:50 -0500

    added components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner'
+import CardContent from './CardContent'
+
+
+function CardContainer(){
+    return(
+        <div className="cardinfo" >
+            <a href="www.reactjs.org"><CardBanner /></a>
+            <a href="www.reactjs.org"><CardContent /></a>
+        </div>
+
+
+    );
+};
+
+export default CardContainer
